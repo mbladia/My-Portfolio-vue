ebe1f13bbe249850791dcaa54f56de49f28826b3
Mark Bryan <mbladia10@gmail.com>
2021-07-20 19:08:01 +0800

    fix

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
-const express = require('express')
-const serveStatic = require('serve-static')
-const path = require('path')
+// const express = require('express')
+// const serveStatic = require('serve-static')
+// const path = require('path')
 
-const app = express()
+// const app = express()
 
-app.use('/', serveStatic(path.join(__dirname, '/dist')))
+// app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
-const port = process.env.PORT || 8080
-app.listen(port)
+// const port = process.env.PORT || 8080
+// app.listen(port)
 
-console.log('Listening on port' + port);
+// console.log('Listening on port' + port);
+
+const http = require('http')
+const fs = require('fs')
+const httpPort = 80
+
+http.createServer((req, res) => {
+  fs.readFile('index.html', 'utf-8', (err, content) => {
+    if (err) {
+      console.log('We cannot open "index.html" file.')
+    }
+
+    res.writeHead(200, {
+      'Content-Type': 'text/html; charset=utf-8'
+    })
+
+    res.end(content)
+  })
+}).listen(httpPort, () => {
+  console.log('Server listening on: http://localhost:%s', httpPort)
+})
