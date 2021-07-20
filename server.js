4d4c584ba1f97afb5758eaaf4a9d5a29a40d0087
Mark Bryan <mbladia10@gmail.com>
2021-07-20 20:07:40 +0800

    fix

--- server.js
+++ server.js
@@ -1,20 +1,14 @@
-// const express = require('express')
-// const serveStatic = require('serve-static')
-// const path = require('path')
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
 
-// const app = express()
+const app = express()
 
-// app.use('/', serveStatic(path.join(__dirname, '/dist')))
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
-// const port = process.env.PORT || 8080
-// app.listen(port)
+const port = process.env.PORT || 8080
+app.listen(port)
 
-// console.log('Listening on port' + port);
+console.log('Listening on port' + port);
 
 
-const express = require('express');
-const history = require('connect-history-api-fallback');const app = express();
-app.use(history());
-app.use(express.static('src'));app.get('/', (req, res) => {
-  res.sendFile('src/index.html');
-});app.listen(3000, () => console.log('server started'));
