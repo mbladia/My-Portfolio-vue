63dd807b56bead31e7cdb79aeb0907d64df049af
Mark Bryan <mbladia10@gmail.com>
2021-07-20 20:09:42 +0800

    fix

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
+var history = require('connect-history-api-fallback');
+
 
 const app = express()
-
+app.use(history());
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
 const port = process.env.PORT || 8080
