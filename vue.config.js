7006eb94ff19fd7c7750d7e6c659322056368e06
Mark Bryan <mbladia10@gmail.com>
2021-07-20 19:15:25 +0800

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,5 @@
 
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-    ? '/production-sub-path/'
-    : '/',
     css: {
         loaderOptions: {
         // pass options to sass-loader
